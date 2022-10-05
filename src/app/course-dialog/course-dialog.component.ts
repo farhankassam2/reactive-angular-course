131f81c1231257d1994bc20dbb8ad6a13338d5ab
Farhan Kassam <farhankassam@dbaresults.com>
2022-10-05 16:21:24 -0500

    Finished implementing behaviour subject store and store optimistic data modifications: lesson 24

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -8,6 +8,7 @@ import {throwError} from 'rxjs';
 import { CoursesService } from '../services/courses.service';
 import { LoadingService } from '../loading/loading.service';
 import { MessagesService } from '../messages/messages.service';
+import { CoursesStore } from '../services/courses.store';
 
 @Component({
     selector: 'course-dialog',
@@ -28,8 +29,7 @@ export class CourseDialogComponent implements AfterViewInit {
         private fb: FormBuilder,
         private dialogRef: MatDialogRef<CourseDialogComponent>,
         @Inject(MAT_DIALOG_DATA) course:Course,
-        private coursesService: CoursesService,
-        private loadingService: LoadingService,
+        private coursesStore: CoursesStore,
         private messagesService: MessagesService) {
 
         this.course = course;
@@ -51,23 +51,27 @@ export class CourseDialogComponent implements AfterViewInit {
         
       const changes = this.form.value;
 
-      const saveCourse$ = this.coursesService.saveCourse(this.course.id, changes)
-      .pipe(
-        catchError((err) => {
-            const message = "Could not save course.";
-            console.log(message, err);
-            this.messagesService.showErrors(message);
-            return throwError(err); //replace parent observable
-        })
-      );
+      this.coursesStore.saveCourse(this.course.id, changes)
+        // Below commented out since error message rendering is useless if dialog is already closed before the message comes along.
+        // .pipe(
+        //     catchError((err) => {
+        //         const message = "Could not save course.";
+        //         console.log(message, err);
+        //         this.messagesService.showErrors(message);
+        //         return throwError(err); //replace parent observable
+        //     })
+        // )
+        .subscribe();
+
+      this.dialogRef.close(changes); // closing dialog while save is ongoing in the background via a network call.
       
       // Below subscribes to saveCourse$ observable and returns val from saveCourse$
-      this.loadingService.showLoaderUntilCompleted(saveCourse$) 
-        .subscribe(
-            (val) => {
-                this.dialogRef.close(val);
-            }
-        );
+    //   this.loadingService.showLoaderUntilCompleted(saveCourse$) 
+    //     .subscribe(
+    //         (val) => {
+    //             this.dialogRef.close(val);
+    //         }
+    //     );
 
     }
 
--- src/app/services/courses.store.ts
+++ src/app/services/courses.store.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { BehaviorSubject, Observable, throwError } from "rxjs";
-import { catchError, map, tap } from "rxjs/operators";
+import { catchError, map, shareReplay, tap } from "rxjs/operators";
 import { LoadingService } from "../loading/loading.service";
 import { MessagesService } from "../messages/messages.service";
 import { Course, sortCoursesBySeqNo } from "../model/course";
@@ -11,13 +11,13 @@ import { Course, sortCoursesBySeqNo } from "../model/course";
 })
 export class CoursesStore {
 
-    private subject = new BehaviorSubject<Course[]>([]);
+    private subject = new BehaviorSubject<Course[]>([]); // BehaviourSubject has reference to current list of courses somewhere
 
     courses$: Observable<Course[]> = this.subject.asObservable(); // anywhere can subscribe to this observable without knowing where it is coming from. But they know that it contains the latest courses list.
 
     constructor(private http: HttpClient, 
                 private loading: LoadingService, 
-                private messages: MessagesService) {
+                private messagesService: MessagesService) {
         this.loadAllCourses(); // all courses loaded once at application startup.
     }
 
@@ -27,7 +27,7 @@ export class CoursesStore {
                 map(response => response["payload"]),
                 catchError(err => {
                     const message = "Could not load courses";
-                    this.messages.showErrors(message);
+                    this.messagesService.showErrors(message);
                     console.log(message, err);
                     return throwError(err); // terminates observable chain
                 }),
@@ -36,6 +36,32 @@ export class CoursesStore {
         
         this.loading.showLoaderUntilCompleted(loadCourses$)
             .subscribe(); // loading indicator when we subscribe to output observable and loading finishes when observable finishes its lifecycle.
+    }
+
+    saveCourse(courseId: string, changes: Partial<Course>): Observable<any> {
+        // UPDATE CLIENT:
+        const courses: Course[] = this.subject.getValue(); // list of courses last emitted by subject
+        const index = courses.findIndex(course => course.id === courseId);
+        const newCourse: Course = {
+                ...courses[index],
+                ...changes
+        };
+        const newCourses: Course[] = courses.slice(0); // creates complete copy of courses
+        newCourses[index] = newCourse;
+
+        this.subject.next(newCourses); // user interface then will immediately reflect changes
+
+        // SAVE TO SERVER: without loading indicator in UI.
+        return this.http.put(`/api/courses/${courseId}`, changes)
+            .pipe(
+                catchError(err => {
+                    const message = "Could not save course";
+                    console.log(message, err);
+                    this.messagesService.showErrors(message);
+                    return throwError(err);
+                }),
+                shareReplay() // so that multiple subscriptions to saveCourse() does not trigger multiple API calls
+            )
     }
     
     filterByCategory(category: string): Observable<Course[]> {
