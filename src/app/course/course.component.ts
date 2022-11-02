ad0318ae608fd01318651b54a121b64f0550d4bd
Farhan Kassam <farhankassam@dbaresults.com>
2022-11-02 13:26:01 -0500

    committing code changes from lessons 36 to the last lesson, 38.

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {Course} from '../model/course';
 import {
@@ -13,21 +13,27 @@ import {
   withLatestFrom,
   concatAll, shareReplay, catchError
 } from 'rxjs/operators';
-import {merge, fromEvent, Observable, concat, throwError} from 'rxjs';
+import {merge, fromEvent, Observable, concat, throwError, combineLatest} from 'rxjs';
 import {Lesson} from '../model/lesson';
 import { CoursesService } from '../services/courses.service';
 
-
+interface CourseData {
+  course: Course;
+  lessons: Lesson[];
+}
 @Component({
   selector: 'course',
   templateUrl: './course.component.html',
-  styleUrls: ['./course.component.css']
+  styleUrls: ['./course.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CourseComponent implements OnInit {
 
-  course$: Observable<Course>;
+  data$: Observable<CourseData>;
 
-  lessons$: Observable<Lesson[]>;
+  // course$: Observable<Course>;
+
+  // lessons$: Observable<Lesson[]>;
 
   constructor(private route: ActivatedRoute,
               private coursesService: CoursesService) {
@@ -38,21 +44,33 @@ export class CourseComponent implements OnInit {
   ngOnInit() {
       const courseId = parseInt(this.route.snapshot.paramMap.get('courseId'));
 
-      this.course$ = this.coursesService.loadCourseById(courseId);
+      const course$ = this.coursesService.loadCourseById(courseId) 
+        .pipe(
+          startWith(null) // forces course observable to first emit null
+        )
 
-      this.lessons$ = this.coursesService.loadAllCourseLessons(courseId);
+      const lessons$ = this.coursesService.loadAllCourseLessons(courseId)
+        .pipe(
+          startWith([]) // forces lessons observable to first emit an empty array
+        )
+
+      // combines two or more observables into one
+      // returns tuples of data returned from multiple observables
+      // has a slightly different logic for first value emmitted: waits for both to emit first value then it emits a combined first value
+      this.data$ = combineLatest([course$, lessons$]) 
+        .pipe(
+          map(([course, lessons]) => {
+            return {
+              course, 
+              lessons
+            };
+          }),
+          tap(console.log) // logs all values emmitted from observable subscription
+        )
   }
 
 
 }
-
-
-
-
-
-
-
-
 
 
 
